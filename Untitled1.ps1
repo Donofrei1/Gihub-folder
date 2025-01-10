826a3cb60a215f2f86b7127f1b20cca1e4d216aa
Donofrei1 <123denisgorohovschi@gmail.com>
2025-01-09 21:26:02 -0800

    Expired accounts

--- Untitled1.ps1
+++ Untitled1.ps1
@@ -1,1 +1,11 @@
-﻿
+﻿# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Define the time range for the last 24 hours
+$yesterday = (Get-Date).AddDays(-1)
+
+# Get user accounts that expired within the last 24 hours
+$expiredUsers = Get-ADUser -Filter {AccountExpirationDate -gt $yesterday -and AccountExpirationDate -le (Get-Date)} -Properties AccountExpirationDate
+
+# Display the expired user accounts
+$expiredUsers | Select-Object Name, SamAccountName, AccountExpirationDate
